2f0aa4e85339b843aea897baf53a064a0ff8efbf
Hackbright Student <info@hackbrightacademy.com>
2015-07-09 17:13:35 -0700

    turned the original markov code into a class

--- markov.py
+++ markov.py
@@ -4,12 +4,14 @@ from random import choice
 
 class SimpleMarkovGenerator(object):
 
-    def read_files(self, filenames):
+
+    def read_files(self, file_list): #do we need a second argument here for file name(s)?
         """Given a list of files, make chains from them.
-        Half(ish) of the original first function from non-oo
+        Currently only taking the first file specified in the terminal
         """
         text_string = ""
-        for file_path in filenames:
+        #files_to_string = list(sys.argv[1:])
+        for file_path in file_list:
             read_the_file = open(file_path)
             for line in read_the_file:
                 text_string += line.rstrip("\n") + " "
@@ -18,33 +20,39 @@ class SimpleMarkovGenerator(object):
         return text_string
 
 
-    def make_chains(self, corpus):
+    def make_chains(self, corpus): #just need to sort out opening the string, 
+    #splitting string into list of words
         """Takes input text as string; stores chains.
+        Need to take the text_string from read_files, and need to supply
+        the file name when we call that method to start this method. 
 
         """
-
+        #corpus = self.read_files()
         chains = {}
 
-            words = corpus.split()
+        words = corpus.split()
 
-            for i in range(len(words) - 2):
-                key = (words[i], words[i + 1])
-                value = words[i + 2]
+        for i in range(len(words) - 2):
+            key = (words[i], words[i + 1])
+            value = words[i + 2]
 
-                if key not in chains:
-                    chains[key] = []
+            if key not in chains:
+                chains[key] = []
 
-                chains[key].append(value)
-
+            chains[key].append(value)
+        return chains
         # your code here
         # ultimately returns a dictionary
-        # stores chains (as opposed to creating them)
+        # stores chainsSimpleMarkovGenerator (as opposed to creating them)
         # calls read_files before getting into its primary function
 
     def make_text(self, chains):
         """Takes dictionary of markov chains; returns random text."""
+        #chains = self.make_chains()
         key = choice(chains.keys())
+        #print key
         words = [key[0], key[1]]
+        #print words
         while key in chains:
         # Keep looping until we have a key that isn't in the chains
         # (which would mean it was the end of our original text)
@@ -66,4 +74,10 @@ if __name__ == "__main__":
     # we should call the read_files method with the list of filenames
     # we should call the make_text method 5x
 
-    pass
+    file_list = sys.argv[1:]
+
+    random_sentence = SimpleMarkovGenerator()
+    concat_string = random_sentence.read_files(file_list) 
+    markov_dict = random_sentence.make_chains(concat_string)
+    print random_sentence.make_text(markov_dict)
+
